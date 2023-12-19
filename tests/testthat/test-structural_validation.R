99f43cbfaba94436a9bbaa82c19bc76e86fdf36f
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-12-19 12:44:26 -0800

    🛑🧪 Fail tests

--- tests/testthat/test-structural_validation.R
+++ tests/testthat/test-structural_validation.R
@@ -1,6 +1,10 @@
 describe("Validate structure", {
   it("write structural errors", {
     csv_path <- "/workdir/tests/data/fail_structural_validation.csv"
+    output_file <- "/workdir/structural_errors.txt"
+    testtools::delete_output_file(output_file)
     check_structure(csv_path)
+    expect_true(testtools::exist_output_file(output_file))
+    testtools::delete_output_file(output_file)
   })
 })

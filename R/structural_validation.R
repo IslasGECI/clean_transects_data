7c4530f00561231db3a34d032447d8bce4b8d1fc
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-12-19 14:14:17 -0800

    ✅ Pass tests

--- R/structural_validation.R
+++ R/structural_validation.R
@@ -5,8 +5,10 @@ check_structure <- function(csv_path) {
   system(glue::glue("goodtables /workdir/tests/data/datapackage.json > {errors_path}"))
   testtools::delete_output_file(temporal_csv)
   error_count_string <- system(glue::glue("cat {errors_path} | grep error-count"), intern = TRUE)
-  is_valid <- all(stringr::str_detect(error_count_string, "'error-count': 0"))
+  is_valid <- all(stringr::str_detect(error_count_string, "0"))
   if (is_valid) {
     testtools::delete_output_file(errors_path)
+  } else {
+    stop("🔴🚨 Structural error. Check file structural_errors.txt for details")
   }
 }

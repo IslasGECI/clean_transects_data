606c4b460a8eac09241fea5d5907154ef5a381f0
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-12-20 14:43:14 -0800

    ✅ Pass tests

--- R/split_rodent_table.R
+++ R/split_rodent_table.R
@@ -3,3 +3,8 @@ get_rodent_traps_table <- function(raw_data) {
     dplyr::select(c("Sitio", "# Transecto", "Tipo de vegetación o cobertura", "Trampa")) |>
     unique()
 }
+
+get_rodent_captures <- function(raw_data) {
+  raw_data |>
+    dplyr::select(-"Tipo de vegetación o cobertura")
+}

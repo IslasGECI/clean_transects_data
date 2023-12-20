fd0da5b55bc408391c39410c2e6f7a9c0bebf7eb
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-12-20 11:27:06 -0800

    ✅ Pass tests

--- R/split_transect_records.R
+++ R/split_transect_records.R
@@ -1,3 +1,5 @@
 get_site_table <- function(raw_data) {
-  raw_data[1:17, ]
+  raw_data |>
+    dplyr::select(c("Sitio", "Clave Unidad de Muestreo", "Longitud del transecto (m)", "Tipo de vegetación o cobertura", "# punto o transecto")) |>
+    dplyr::distinct()
 }

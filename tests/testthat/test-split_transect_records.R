03c1f9c0375d7b58ce8843098b5aab986a7c495d
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-12-20 11:43:33 -0800

    🛑🧪 Fail tests

--- tests/testthat/test-split_transect_records.R
+++ tests/testthat/test-split_transect_records.R
@@ -1,6 +1,6 @@
 describe("Split transcet csv to higher normal forms", {
+  complete_data <- readr::read_csv("/workdir/tests/data/Base_datos_aves_para_CD.csv", show_col_types = FALSE)
   it("Split site table", {
-    complete_data <- readr::read_csv("/workdir/tests/data/Base_datos_aves_para_CD.csv", show_col_types = FALSE)
     obtained <- get_site_table(complete_data)
     obtained_rows <- nrow(obtained)
     expected_rows <- 17
@@ -18,5 +18,11 @@ describe("Split transcet csv to higher normal forms", {
       length()
     expected_number_claves <- 7
     expect_equal(obtained_number_claves, expected_number_claves)
+  })
+  it("Split record events", {
+    obtained <- split_count_records(complete_data)
+    obtained_number_cols <- ncol(obtained)
+    expected_number_cols <- 10
+    expect_equal(obtained_number_cols, expected_number_cols)
   })
 })
